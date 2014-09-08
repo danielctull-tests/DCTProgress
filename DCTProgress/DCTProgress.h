0b31e076794a1af095851d153090977aadf89cbe
Daniel Tull <dt@danieltull.co.uk>
2014-09-08 15:54:03 +0100

    Initial Commit

---
+++ DCTProgress/DCTProgress.h
@@ -0,0 +1,19 @@
+//
+//  DCTProgress.h
+//  DCTProgress
+//
+//  Created by Daniel Tull on 08/09/2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DCTProgress.
+FOUNDATION_EXPORT double DCTProgressVersionNumber;
+
+//! Project version string for DCTProgress.
+FOUNDATION_EXPORT const unsigned char DCTProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCTProgress/PublicHeader.h>
+
+
